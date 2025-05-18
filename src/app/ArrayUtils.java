cd4a29d53c8d54d69e9643fe4e6ae3f3a6dbbe1b
melmanss <cfhfyjdbx@gmail.com>
2025-05-18 12:10:08 +0300

    New Annotation Version

--- src/app/ArrayUtils.java
+++ src/app/ArrayUtils.java
@@ -4,6 +4,7 @@ public class ArrayUtils {
 
     @MethodInfo(name = "sortArray", returnType = "int[]", description = "Сортує масив цілих чисел.")
     @Author(name = "Олександр", surname = "")
+    @Version("1.0")
     public int[] sortArray(int[] array) {
 
         java.util.Arrays.sort(array);
@@ -12,6 +13,7 @@ public class ArrayUtils {
 
     @MethodInfo(name = "findMax", returnType = "int", description = "Знаходить максимальне значення в масиві.")
     @Author(name = "Олександр", surname = "")
+    @Version("1.1")
     public int findMax(int[] array) {
 
         int max = array[0];
